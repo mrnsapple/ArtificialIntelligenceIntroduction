29d92bf36126cb4277089d4f57312e4cefb5aeff
Oriol Manzano <oriol.manzano@epitech.eu>
2021-10-06 19:50:02 +0200

    [ADD] modified minmaxalgorithm

--- minmax_src/MinMaxAlgorithm.py
+++ minmax_src/MinMaxAlgorithm.py
@@ -4,17 +4,44 @@ from ArtificialIntelligenceIntroduction.src.node import Node
 from ArtificialIntelligenceIntroduction.src.problem import Problem
 from ArtificialIntelligenceIntroduction.src.algorithm import Algorithm, AlgorithmException
 import random
+from enum import Enum
+class NodeState(Enum):
+    Max=1
+    Min=2
+    Final=3
 
 class MinMaxAlgorithm(Algorithm):
+    def __init__(self) -> None:
+        self.depth = 4
+        self.final_nodes = []
+    
+    #call again problem until reached a final node in all branches
+    def addChild(self, problem: MinMaxProblem, node: Node):
+        #check node is  final, meaning the node is end of current player turn
+        if node.data["node_state"] is "final":
+            self.final_nodes.append(node)
+            return 
+        problem.getSuccessors(node)
+        [self.addChild(child) for child in node.childs]
+        return 
+
+    #check which node gives better value either min or max and return it
+    def get_best_choice(self, node):
+        state = node.data["node_state"]
+        if not self.final_nodes:
+            return node
+        best_node = self.final_nodes[0]
+        for node in self.final_nodes:
+            if state == "min" and node.data["heuristic"] < best_node:
+                best_node = node
+            elif state == "max" and node.data["heuristic"] > best_node:
+                best_node = node
+        return best_node
+
+    #give the node that has the better result to throw in this turn
     def get_successor(self, problem: MinMaxProblem, node: Node) -> Node:
         if not node or not problem:
             raise AlgorithmException("Node or problem does not exist")
-        data = node.data["data"]
-        game_state =  node.data["game state"]
-        problem.getSuccessors(node)
-        response_index = random.randint(0, len(data)-1)
-        print("\n\n")
-        print(node.data)
-        print(data)
-        print(game_state)
-        return Node(data=response_index, parent=node, childs=[], tree_lvl=node.tree_lvl+1, is_visited=False)
+        self.final_nodes = []
+        self.addChild(problem,node)
+        return self.get_best_choice(node)
