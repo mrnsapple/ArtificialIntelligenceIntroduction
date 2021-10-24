050c9db97393fe6a490c587fdd6d88f3c22a4457
Oriol Manzano <oriol.manzano@epitech.eu>
2021-10-24 11:01:37 +0200

    [ADD] included server and src/ server folder to repository

--- scripts/runServer.sh
+++ scripts/runServer.sh
@@ -1,1 +1,1 @@
-python3.6 /home/oriol/Epitech_projects/5_Year/AI/phantom_opera/server.py
+python3.6 ./server.py
--- scripts/runproject.sh
+++ scripts/runproject.sh
@@ -1,4 +1,4 @@
-/home/oriol/Epitech_projects/5_Year/AI/ArtificialIntelligenceIntroduction/scripts/runServer.sh &
+./scripts/runServer.sh &
 sleep 1
-/home/oriol/Epitech_projects/5_Year/AI/ArtificialIntelligenceIntroduction/scripts/runClient.sh &
+./scripts/runClient.sh &
 #/home/oriol/Epitech_projects/5_Year/AI/ArtificialIntelligenceIntroduction/scripts/runClient.sh &
